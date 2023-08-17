06317012612ebfc1153bac114c226aec286d27a2
Yasir Jehangir <iamyasirniazi@gmail.com>
2023-08-17 13:27:42 +0500

    first commit

--- javascript.js
+++ javascript.js
@@ -8,7 +8,55 @@ $(document).ready(function () {
   $("#slider").owlCarousel({
     loop: true,
     margin: 0,
-    //  nav:true,
+     nav:false,
+    center: true,
+    stagePadding: 0,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    // navText: ["<i class='ri-arrow-left-s-line'></i>", "<i class='ri-arrow-right-s-line'></i>"],
+    smartSpeed: 1000,
+    autoplayHoverPause: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0,
+        // dots: false,
+        // nav: true
+      },
+      575: {
+        items: 1,
+        stagePadding: 0,
+      },
+      768: {
+        items: 1,
+        stagePadding: 0,
+      },
+      992: {
+        items: 1,
+        stagePadding: 0,
+      },
+
+      1200: {
+        items: 1,
+        stagePadding: 0,
+      },
+
+      1400: {
+        items: 1,
+        stagePadding: 0,
+      },
+    },
+  });
+});
+
+// Carousel Message Slider Code
+$(document).ready(function () {
+  $("#messageSlider").owlCarousel({
+    loop: true,
+    margin: 0,
+     nav:false,
     center: true,
     stagePadding: 0,
     dots: true,
