a81d47b673fc15e8f7d605fc763573aa60216547
unknown <iamyasirniazi@gmail.com>
2023-10-25 23:24:51 -0700

    homepage hospitals and news events slider added

--- javascript.js
+++ javascript.js
@@ -51,6 +51,102 @@ $(document).ready(function () {
   });
 });
 
+// Events Carousel Code
+$(document).ready(function () {
+  $("#events-slider").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav:false,
+    center: true,
+    stagePadding: 0,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    // navText: ["<i class='ri-arrow-left-s-line'></i>", "<i class='ri-arrow-right-s-line'></i>"],
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0,
+        // dots: false,
+        // nav: true
+      },
+      575: {
+        items: 1,
+        stagePadding: 0,
+      },
+      768: {
+        items: 2,
+        stagePadding: 0,
+      },
+      992: {
+        items: 4,
+        stagePadding: 0,
+      },
+
+      1200: {
+        items: 4,
+        stagePadding: 0,
+      },
+
+      1400: {
+        items: 4,
+        stagePadding: 0,
+      },
+    },
+  });
+});
+
+// News Carousel Code
+$(document).ready(function () {
+  $("#news-slider").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav:false,
+    center: true,
+    stagePadding: 0,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    // navText: ["<i class='ri-arrow-left-s-line'></i>", "<i class='ri-arrow-right-s-line'></i>"],
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0,
+        // dots: false,
+        // nav: true
+      },
+      575: {
+        items: 1,
+        stagePadding: 0,
+      },
+      768: {
+        items: 2,
+        stagePadding: 0,
+      },
+      992: {
+        items: 4,
+        stagePadding: 0,
+      },
+
+      1200: {
+        items: 4,
+        stagePadding: 0,
+      },
+
+      1400: {
+        items: 4,
+        stagePadding: 0,
+      },
+    },
+  });
+});
+
 // Carousel Message Slider Code
 $(document).ready(function () {
   $("#messageSlider").owlCarousel({
